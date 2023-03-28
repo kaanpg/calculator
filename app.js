5871f1b780dcf37d5499133b7a9662f5a57259f2
Kaan Piri Gürsoy <36899323+kaanpg@users.noreply.github.com>
2023-03-29 00:34:08 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function () {
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
+  let operators = document.querySelector(".btn-operator");
   let clear = document.querySelector(".btn-clear");
   let equal = document.querySelector(".btn-equal");
 
@@ -12,12 +13,19 @@
   });
 
   equal.addEventListener("click", function (e) {
-    if (screen.value === "") {
-      screen.value = "";
-    } else {
+    if (screen.value !== "") {
       let answer = eval(screen.value);
       screen.value = answer;
     }
+  });
+
+  operators.addEventListener("click", function (e) {
+    const operatorsArr = ["+", "-", "/", "*"];
+    let len = screen.value.length;
+    let lastchar = screen.value.substring(len - 1, len);
+    if (operatorsArr.includes(lastchar)) {
+    }
+    // screen.value lastchari yeni operatorle degis
   });
 
   clear.addEventListener("click", function (e) {
